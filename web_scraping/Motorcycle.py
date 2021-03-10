72dd8d6f9614b6e1d728dc40f047d93831c481f8
Gabriel Flynn <gm.flynn99@yahoo.com>
2021-03-10 05:36:51 -0600

    Created Motorcycle and Review model

---
+++ web_scraping/Motorcycle.py
@@ -0,0 +1,37 @@
+class Motorcycle:
+
+    """
+    :param make: manufacturer of the motorcycle
+    :param model: model of the motorcycle
+    :param year_start: start of this generation of motorcycle
+    :param year_end: end of this generation of motorcycle
+    :param price: price of this motorcycle if I were to go out and buy it today (based off kbb values)
+    :param category: type of motorcycle (sport bike, adventure bike, naked bike, etc.)
+    :param is_new: is a bike that is currently being produced/manufactured - might remove this later as this might complicate things a bit
+    :param engine_size: size of the engine ub cubic centimeters
+    :param engine_type: engine type - might just keep this at v4, i4, i2, v-twin, etc. for now
+    :param insurance_group: group from 1 to 17 (17 being the most expensive) the motorcycle is in
+    :param mpg: mpg of the motorcycle
+    :param tank_range: range the motorcycle can go on one tank
+    :param power: bhp of the bike
+    :param seat_height: seat height of the bike (in inches)
+    :param weight: weight of the bike
+    :param review: Review associated with the bike
+    """
+    def __init__(self, make, model, year_start, year_end, price, category, is_new, engine_size, engine_type, insurance_group, mpg, tank_range, power, seat_height, weight, review):
+        self.review = review
+        self.weight = weight
+        self.seat_height = seat_height
+        self.power = power
+        self.range = tank_range
+        self.mpg = mpg
+        self.insurance_group = insurance_group
+        self.engine_type = engine_type
+        self.engine_size = engine_size
+        self.make = make
+        self.model = model
+        self.year_start = year_start
+        self.year_end = year_end
+        self.price = price
+        self.category = category
+        self.is_new = is_new
---
+++ web_scraping/Review.py
@@ -0,0 +1,30 @@
+class Review:
+
+    """
+    :param overall_rating: overall review rating of the bike
+    :param overall_rating_review_text: text listed under the overall rating review
+    :param ride_quality: score of the ride quality and brakes
+    :param ride_quality_review_text: text listed under the ride quality review
+    :param engine: score of the engine category in the review
+    :param engine_review_text: text listed under the engine review
+    :param reliability: score of the reliability and build quality category in the review
+    :param reliability_review_text: text listed under the reliability review
+    :param value: score of the value vs rivals
+    :param value_review_text: text listed under the value review
+    :param equipment: score of the equipment category in the review
+    :param equipment_review_text: text listed under the equipment review
+    """
+    def __init__(self, overall_rating, overall_rating_review_text, ride_quality, ride_quality_review_text, ride_quality_text, engine, engine_review_text, reliability, reliability_review_text, value, value_review_text, equipment, equipment_review_text):
+        self.equipment_review_text = equipment_review_text
+        self.equipment = equipment
+        self.value_review_text = value_review_text
+        self.value = value
+        self.reliability_review_text = reliability_review_text
+        self.reliability = reliability
+        self.engine_review_text = engine_review_text
+        self.engine = engine
+        self.ride_quality_text = ride_quality_text
+        self.ride_quality_review_text = ride_quality_review_text
+        self.ride_quality = ride_quality
+        self.overall_rating_review_text = overall_rating_review_text
+        self.overall_rating = overall_rating
